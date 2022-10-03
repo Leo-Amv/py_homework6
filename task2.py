5cd7d4f37b1962408657773e21986ad93773a2df
Leo_Amv <leonidamvrosov@yandex.ru>
2022-10-03 16:32:39 +0500

    task2 completed!

--- task2.py
+++ task2.py
@@ -1,1 +1,12 @@
 # Мимикрия
+def transformation(x): return x
+# transformation = lambda x : x
+
+
+values = [1, 23, 42, 'asdfg']
+transformed_values = list(map(lambda x: x, values))
+result = list(map(transformation, values))
+if values == transformed_values == result:
+    print('ok')
+else:
+    print('fail')
