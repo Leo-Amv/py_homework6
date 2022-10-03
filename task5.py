53bd8377011501ddaabafb6681096d2fd4bfc844
Leo_Amv <leonidamvrosov@yandex.ru>
2022-10-03 20:02:35 +0500

    task5 completed!

--- task5.py
+++ task5.py
@@ -1,1 +1,17 @@
 # Все равны, как на подбор
+
+values = [i for i in range(20) if i % 2 == 0]
+qubes = [i**3 for i in range(10) if i % 3 != 0]
+
+print(values)
+print(qubes)
+
+
+def same_by(characteristic, objects):
+    return len(set(map(characteristic, objects))) == 1
+
+
+if same_by(lambda x: x % 2, values):
+    print('same')
+else:
+    print('defferent')
