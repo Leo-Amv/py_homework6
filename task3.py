b97eaeeab5302f0ba221de0e721689505ae48085
Leo_Amv <leonidamvrosov@yandex.ru>
2022-10-03 17:40:36 +0500

    task3 completed!

--- task3.py
+++ task3.py
@@ -1,1 +1,10 @@
 # Самая далекая планета
+
+def find_farthest_orbit(list_of_orbits):
+    return max(list_of_orbits, key=lambda x: x[0] * x[1] if x[0] != x[1] else False)
+
+
+orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+
+
+print(*find_farthest_orbit(orbits))
